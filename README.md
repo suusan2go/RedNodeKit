429c9ff434fc9b843b3cb5bdb1ed77df0212880a
suzan2go <ksuzuki180@gmail.com>
2014-11-25 00:12:59 +0900

    add updating tickets function

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-RedNodeKit
+rednode
 =======
 
 simple redmine client (builted by node-webkit)
 
 #Function
-* You can create issues to your redmine by RedNodeKit.
-* Projects , members and trackers are automatically obtained from your redmine by RedNodeKit.
+* You can create and update issues to your redmine by this tool.
+* Projects , members and trackers are automatically obtained from your redmine by this tool.
 * Japanese version only now.
 
 #installation 
 This app only support Mac OS X.(but you can use your platform by copying  files and download your platform node-webkit.)
 
-Download from [releases/latest](https://github.com/suzan2go/rednode/releases/latest)
+Download from releases/latest
 
 #Usage
 Download this app and click it.
--- js/rest.js
+++ js/rest.js
@@ -93,3 +93,95 @@ function createIssue(redmine_url,apikey,issue){
                     }
   });
 }
+
+function updateIssue(redmine_url,apikey,issue,issue_id){
+
+  return $.ajax({
+    type : "PUT",
+         url : redmine_url  +  "/issues/" + issue_id + ".json?key=" + apikey,
+         async : false,
+         contentType : 'application/json',
+         dataType : "json",
+         data : JSON.stringify(issue),
+         success : function(data) {
+           console.log("updateIssue:succeed");
+           if (data == null || data == undefined) {
+             console.log("updateIssue:Transaction error. ");
+           }
+         },
+         error : function(XMLHttpRequest, textStatus, errorThrown) { //
+                   console.log(errorThrown);
+                   console.log("updateIssue:Server Error . Pleasy try again later.");
+                 },
+         complete : function() {
+                    }
+  });
+}
+
+
+function getIssues(redmine_url,apikey){
+
+  return $.ajax({
+    type : "GET",
+         url : redmine_url  +  "/issues.json?assigned_to_id=me&key=" + apikey,
+         contentType : 'text/plain; charset=utf-8',
+         dataType : "json",
+         success : function(data) {
+
+           if (data == null || data == undefined) {
+             console.log("getIssues:Transaction error. ");
+             return;
+           }
+         },
+         error : function(XMLHttpRequest, textStatus, errorThrown) { //
+                   console.log("getIssues:Server Error . Pleasy try again later.");
+                 },
+         complete : function() {
+                    }
+  });
+}
+
+function getIssue(redmine_url, apikey, id){
+
+  return $.ajax({
+    type : "GET",
+         url : redmine_url  +  "/issues/" + id  + ".json?key=" + apikey + "&include=journals",
+         contentType : 'text/plain; charset=utf-8',
+         dataType : "json",
+         success : function(data) {
+
+           if (data == null || data == undefined) {
+             console.log("getIssue:Transaction error. ");
+             return;
+           }
+         },
+         error : function(XMLHttpRequest, textStatus, errorThrown) { //
+                   console.log("getIssue:Server Error . Pleasy try again later.");
+                 },
+         complete : function() {
+                    }
+  });
+}
+
+//get issue statuses
+function getStatuses(redmine_url,apikey){
+
+    return $.ajax({
+          type : "GET",
+                    url : redmine_url  +  "/issue_statuses.json",
+                    contentType : 'text/plain; charset=utf-8',
+                    dataType : "json",
+                    success : function(data) {
+
+                                 if (data == null || data == undefined) {
+                                                console.log("getMenmbers:Transaction error. ");
+                                                             return;
+                                                                        }
+                                          },
+                    error : function(XMLHttpRequest, textStatus, errorThrown) { //
+                                                 console.log("getMembers:Server Error . Pleasy try again later." + errorThrown);
+                                                                  },
+                    complete : function() {
+                                                     }
+      });
+}
